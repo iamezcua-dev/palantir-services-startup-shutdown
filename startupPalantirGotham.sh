ec952a9a6e936027840dbaa51dfeaaf7a6495aa6
Isaac Eduardo Amezcua Bustos <iamezcua@dattlas.com>
2017-10-26 17:47:40 -0500

    Add config snippet
    Add script pre-execution configuration code.

---
+++ startupPalantirGotham.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+## Previous variable configuration
+#
+
+# Server IPs
+PALANTIR_SERVER_APP01=""
+PALANTIR_SERVER_APP02=""
+PALANTIR_SERVER_DB01=""
+
+# Palantir Enterprise Manager application config
+PEM_HOME=""
+PEM_SERVICES_HOME=""
+PEM_TASK_HOME=""
+PEM_BIN_HOME=""
+
+# Palantir Base Realm config
+DB_CTL_EXEC=""
